e809408df178ed14721d5336945155dd88fd2017
David Goulette <dpgoulette@gmail.com>
2014-11-07 14:49:10 -0800

    Fixed comments and variable naming.

--- EpsilonProject3d.m
+++ EpsilonProject3d.m
@@ -21,27 +21,40 @@ DT=DelaunayTri(flatdata);
 % %I think I need to remove any data point that has a voronoi cell that goes
 % %to infinity.  Then remove any tetra that used one of these points.
 
-%find bad voronoi vertices.  Infinity is bad.  And any voronoi vertex
-%outside of the data space.  Thus it is not contained in any delaunay
-%tetra.
-ID = [NaN; pointLocation(DT,VV(2:length(VV),:))];
-BadVV = find(isnan(ID));
+% In our work we choose to remove data that is on the boundary of the data
+% space.  We do this by removing any data point that has a voronoi cell
+% vertex which we deem to be a "bad Voronoi vertex."  We also remove any
+% Infinity is bad because any voronoi cell atteched to it has infinite
+% area/volume.  Also any voronoi vertex outside of the data space is bad
+% because they are often artificially elongated, thus the voronoi cell is
+% less reliable.
 
-%find the vertices in the data that have a voronoi cell which includes a
-%bad voronoi vertex
-BadData = zeros(length(DT.X),1);
+% Note, any finite voronoi vertex that is outside of the dataspace
+% will not be inside any Delaunay tetra.
+
+% find the indices of the bad Voronoi vertices.  pointLocation returns a nan
+% if the voronoi vertex is NOT interior to any triangle/tetra.  We put a NaN
+% in the first slot because the point at infinity is listed first in the
+% Voronoi diagram.
+ID = [NaN; pointLocation(DT,VV(2:length(VV),:))];
+BadVV = find(isnan(ID));%creates index vector of bad vor verts
+
+% Now we find the bad data.  "Bad data" is on the boundary of the data space
+% so it has ind the vertices in the data that have a voronoi cell which
+% includes a bad voronoi vertex.  Create an index of "BadDataID."
+BadDataID = zeros(length(DT.X),1);
 a=1;
 for b=1:length(VC)
     if ~isempty(intersect(BadVV,VC{b}))
-        BadData(a)=b;
+        BadDataID(a)=b;
         a=a+1;
     end
 end
-BadData(BadData==0)=[];
+BadDataID(BadDataID==0)=[];
 
-% %Now we can use this BadData vector to remove any Tetras, Triangles and
-% %Edges that contain any of these bad points.  Then we will only be
-% %considering cells with GoodPoints for inclusion to the epsilon complex.
+%Now we can use this BadData vector to remove any Tetras, Triangles and
+%Edges that contain any of these bad points.  Then we will only be
+%considering cells with GoodPoints for inclusion to the epsilon complex.
 
 %get ALL tets and edges (including bad ones)
 DTtets=DT.Triangulation;
@@ -57,7 +70,7 @@ DTtriangles=unique(DTtriangles,'rows');
 %now remove bad tets, edges and triangles.  They are "bad" if they contain
 %a BadData point.
 
-[R,~]=find(ismember(DTtets,BadData));
+[R,~]=find(ismember(DTtets,BadDataID));
 R=unique(R);
 cells3=DTtets;
 cells3(R,:)=[];
@@ -73,13 +86,13 @@ cells3=[cells3, RCC];
 %find the distance to their nearest common point with EpsilonEdges
 %which returns the cells2 Kx4 matrix with good tris and min dist
 
-[R,~]=find(ismember(DTtriangles,BadData));
+[R,~]=find(ismember(DTtriangles,BadDataID));
 R=unique(R);
 goodTris=DTtriangles;
 goodTris(R,:)=[];
 cells2=EpsilonTwoCells(DT,VV,VC,goodTris);
 
-[R,~]=find(ismember(DTedges,BadData));
+[R,~]=find(ismember(DTedges,BadDataID));
 R=unique(R);
 goodEdges=DTedges;
 goodEdges(R,:)=[];
